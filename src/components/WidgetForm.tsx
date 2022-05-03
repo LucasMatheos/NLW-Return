b222e96d971d0749dd6fb3f859b8714a2e3db84f
Lucas Brito <lucasm_brito@hotmail.com>
2022-05-03 18:03:25 -0300

    FeedBack Bottons

--- src/components/WidgetForm.tsx
+++ src/components/WidgetForm.tsx
@@ -1,6 +1,39 @@
+import { useState } from "react";
+
 import { CloseButton } from "./CloseButton";
+import bugImageUrl from "../assets/bug.svg";
+import ideaImageUrl from "../assets/idea.svg";
+import thoughtImageUrl from "../assets/thought.svg";
+
+const feddbackTypes = {
+  BUG: {
+    title: "Problema",
+    image: {
+      source: bugImageUrl,
+      alt: "Imagem de um inseto",
+    },
+  },
+  IDEA: {
+    title: "Ideia",
+    image: {
+      source: ideaImageUrl,
+      alt: "Imagem de uma lâmpada",
+    },
+  },
+  OTHER: {
+    title: "Outro",
+    image: {
+      source: thoughtImageUrl,
+      alt: "Imagem de um balão de pensamento",
+    },
+  },
+};
+
+type FeedbackType = keyof typeof feddbackTypes;
 
 export function WidgetForm() {
+  const [feedbackType, setFeedbackType] = useState<FeedbackType | null>(null);
+
   return (
     <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
       <header>
@@ -8,7 +41,26 @@ export function WidgetForm() {
         <CloseButton />
       </header>
 
-      <p>hello world</p>
+      {!feedbackType ? (
+        <div className="flex py-8 gap-2 w-full">
+          {Object.entries(feddbackTypes).map(([key, value]) => {
+            return (
+              <button
+                key={key}
+                className="bg-zinc-800 rounded-lg py-5 w-24 flex-1 flex flex-col items-center gap-2 border-2 border-transparent hover:border-brand-500 focus:border-brand-500 focus:outline-none"
+                onClick={() => setFeedbackType(key as FeedbackType)}
+                type="button"
+              >
+                <img src={value.image.source} alt={value.image.alt} />
+                <span>{value.title}</span>
+              </button>
+            );
+          })}
+        </div>
+      ) : (
+        <p>Jelloy</p>
+      )}
+
       <footer className="text-xs text-neutral-400">
         Feito com ♥ pela{" "}
         <a
